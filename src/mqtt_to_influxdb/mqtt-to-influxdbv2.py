343cbb78023819862ab7fcc56f61fd27745ed496
Michael Oberhofer <michael@oberhofer.me>
2025-05-22 08:35:43 +0200

    mqtt_to_influxdb: Added basic device config; removed password placeholder

--- src/mqtt_to_influxdb/mqtt-to-influxdbv2.py
+++ src/mqtt_to_influxdb/mqtt-to-influxdbv2.py
@@ -6,6 +6,7 @@ import json
 import gzip
 import logging
 import os
+import signal
 from influxdb_client.client.influxdb_client_async import InfluxDBClientAsync
 from influxdb_client.client.write_api import WriteApi
 
@@ -21,8 +22,8 @@ if app_env == "development":
 
 MQTT_HOST = os.getenv("PQOPEN_MQTT_HOST", "mqtt.pqopen.com")
 MQTT_PORT = int(os.getenv("PQOPEN_MQTT_PORT", 8883))
-MQTT_USERNAME = os.getenv("PQOPEN_MQTT_USERNAME", "testuser")
-MQTT_PASSWORD = os.getenv("PQOPEN_MQTT_PASSWORD", "testpass")
+MQTT_USERNAME = os.getenv("PQOPEN_MQTT_USERNAME")
+MQTT_PASSWORD = os.getenv("PQOPEN_MQTT_PASSWORD")
 MQTT_TOPIC = os.getenv("PQOPEN_MQTT_TOPIC", "private/#")
 INFLUXDB_URL = os.getenv("PQOPEN_INFLUXDB_URL", "http://localhost:8086")
 INFLUXDB_TOKEN = os.getenv("PQOPEN_INFLUXDB_TOKEN", "")
@@ -38,12 +39,14 @@ def decode_payload(payload: bytes, encoding: str):
 
     return payload_dict
 
-async def mqtt_listener(write_api: WriteApi, device_config: dict):
+async def mqtt_listener(write_api: WriteApi, device_config: dict, stop_event: asyncio.Event):
     """Async: empfängt MQTT-Nachrichten und legt sie in die Queue."""
     tls_context = ssl.create_default_context()
-    async with aiomqtt.Client(MQTT_HOST, port=MQTT_PORT, username=MQTT_USERNAME, password=MQTT_PASSWORD, tls_insecure=False, tls_context=tls_context) as client:
-        await client.subscribe(MQTT_TOPIC)
+    async with aiomqtt.Client(MQTT_HOST, port=MQTT_PORT, username=MQTT_USERNAME, password=MQTT_PASSWORD, tls_insecure=False, tls_context=tls_context, identifier="mqtt-to-database") as client:
+        await client.subscribe(MQTT_TOPIC, 2)
         async for message in client.messages:
+            if stop_event.is_set():
+                break
             try:
                 parts = message.topic.value.split("/")
                 if len(parts) < 4:
@@ -64,23 +67,41 @@ async def mqtt_listener(write_api: WriteApi, device_config: dict):
                                             data_frame_tag_columns=['location_name', "location_lat", "location_lon"])
                         print("data Sent")
                 else:
-                    print("Device not configured")
+                    print("Device not configured", device_id)
             except Exception as e:
                 print("Fehler bei Nachricht:", e)
 
 
 async def main():
-    # DB-Verbindung
+    stop_event = asyncio.Event()
+
+    def shutdown():
+        logger.info("Shutdown signal received.")
+        stop_event.set()
+    
+    loop = asyncio.get_running_loop()
+    for sig in (signal.SIGINT, signal.SIGTERM):
+        loop.add_signal_handler(sig, shutdown)
+
+    # DB-Connection
     db_client = InfluxDBClientAsync(url=INFLUXDB_URL, 
                                     token=INFLUXDB_TOKEN, 
                                     org=INFLUXDB_ORG)
     write_api = db_client.write_api()
+    # Load Device Config
     with open("config/device_config.json") as f:
         device_config = json.load(f)
+
     try:
-        await mqtt_listener(write_api, device_config)
+        await mqtt_listener(write_api, device_config, stop_event)
     except Exception as e:
-        print("MQTT-Fehler:", e)
+        print("MQTT-Error:", e)
+    finally:
+        await db_client.__aexit__(None, None, None)
+        logger.info("Client closed.")
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print("Application stopped by user")
